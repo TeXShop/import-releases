35d30245a76cdd17b525d8329b1561bbd8c4f417
Max Horn <max@quendi.de>
2015-01-26 10:58:54 +0100

    Adapt import-zip

--- import-zips.py
+++ import-zips.py
@@ -12,6 +12,7 @@ from os import popen, path
 from sys import argv, exit, hexversion, stderr
 from time import mktime
 from zipfile import ZipFile
+import re
 
 if hexversion < 0x01060000:
     # The limiter is the zipfile module
@@ -23,23 +24,34 @@ if len(argv) < 2:
     exit(1)
 
 branch_ref = 'refs/heads/import-zips'
-committer_name = 'Z Ip Creator'
-committer_email = 'zip@example.com'
+committer_name = 'Max Horn'
+committer_email = 'max@quendi.de'
 
 fast_import = popen('git fast-import --quiet', 'w')
-def printlines(list):
-    for str in list:
-        fast_import.write(str + "\n")
 
-for zipfile in argv[1:]:
+def println(str):
+    fast_import.write(str + "\n")
+
+def import_zip(zipfile):
     commit_time = 0
     next_mark = 1
     common_prefix = None
     mark = dict()
 
+    basename = path.basename(zipfile)
+    m = re.search('texshopsource-(.+)\.zip', basename)
+    version = m.group(1)
+    tag = 'v' + version
+
+    print " Importing "+zipfile+", version is "+version+"\n"
+
     zip = ZipFile(zipfile, 'r')
     for name in zip.namelist():
         if name.endswith('/'):
+            continue
+        if name.endswith('/.DS_Store'):
+            continue
+        if name.startswith('__MACOSX/'):
             continue
         info = zip.getinfo(name)
 
@@ -55,24 +67,47 @@ for zipfile in argv[1:]:
         mark[name] = ':' + str(next_mark)
         next_mark += 1
 
-        printlines(('blob', 'mark ' + mark[name], \
-                    'data ' + str(info.file_size)))
-        fast_import.write(zip.read(name) + "\n")
+        #print(name)
+        println('blob')
+        println('mark ' + mark[name])
+        println('data ' + str(info.file_size))
+        fast_import.write(zip.read(name))
+        println('')
 
-    committer = committer_name + ' <' + committer_email + '> %d +0000' % \
-        mktime(commit_time + (0, 0, 0))
+    timestamp = mktime(commit_time + (0, 0, 0))
+    committer = '%s <%s> %d +0000' % (committer_name, committer_email, timestamp)
 
-    printlines(('commit ' + branch_ref, 'committer ' + committer, \
-        'data <<EOM', 'Imported from ' + zipfile + '.', 'EOM', \
-        '', 'deleteall'))
+    println('commit ' + branch_ref)
+    println('committer ' + committer)
+    println('data <<EOM')
+    println('Version ' + version)
+    println('EOM')
+    println('')
 
+    println('deleteall')
     for name in mark.keys():
         fast_import.write('M 100644 ' + mark[name] + ' ' +
             name[len(common_prefix):] + "\n")
+    println('')
 
-    printlines(('',  'tag ' + path.basename(zipfile), \
-        'from ' + branch_ref, 'tagger ' + committer, \
-        'data <<EOM', 'Package ' + zipfile, 'EOM', ''))
+    # Create annotated tag
+    # TODO: use 'reset' instead of light-weight tag?
+    #   reset refs/tags/938
+    #   from :938     resp 'from $branch_ref'
+    # TODO: use current date?
+#     println('tag ' + tag)
+#     println('from ' + branch_ref)
+#     println('tagger ' + committer)
+#     println('data <<EOM')
+#     println('Package ' + basename)
+#     println('EOM')
+#     println('')
+    println('reset refs/tags/' + tag)
+    println('from ' + branch_ref)
+    println('')
+
+for zipfile in argv[1:]:
+    import_zip(zipfile)
 
 if fast_import.close():
-    exit(1)
+   exit(1)
